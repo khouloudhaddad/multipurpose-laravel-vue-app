d88a478697427d6b2e0e3b7b3d0b8e78b3b00b6a
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-06-10 15:18:16 +0100

    Vue Router for Navigation

---
+++ resources/js/pages/profile/UpdateProfile.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Profile</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Profile</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="content">
+        <div class="container-fluid">
+
+        </div>
+    </div>
+</template>
+
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,10 +1,33 @@
 import Dashboard from './components/Dashboard.vue'
-
+import ListAppointments from './pages/appointments/ListAppointments.vue'
+import ListUsers from "./pages/Users/ListUsers.vue";
+import UpdateSettings from './pages/settings/UpdateSettings.vue';
+import UpdateProfile from './pages/profile/UpdateProfile.vue';
 export default [
     {
-        path: '/admin/dashboard',
-        name: 'admin.dashboard',
-        component: Dashboard
-    }
+        path: "/admin/dashboard",
+        name: "admin.dashboard",
+        component: Dashboard,
+    },
+    {
+        path: "/admin/appointments",
+        name: "admin.appointments",
+        component: ListAppointments,
+    },
+    {
+        path: "/admin/users",
+        name: "admin.users",
+        component: ListUsers,
+    },
+    {
+        path: "/admin/settings",
+        name: "admin.settings",
+        component: UpdateSettings,
+    },
+    {
+        path: "/admin/profile",
+        name: "admin.profile",
+        component: UpdateProfile,
+    },
 ];
 

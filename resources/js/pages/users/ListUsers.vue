52e78cad256390fa23974117a6d54b7e2fb100f7
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-06-11 13:48:47 +0000

    Format date version 1

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -123,6 +123,15 @@
         })
     }
 
+    /**
+     * Format a date
+     * @param {dateString} dateString
+     */
+     const formatDate =(dateString)=> {
+            const date = new Date(dateString);
+            return new Intl.DateTimeFormat('default', {dateStyle: 'long'}).format(date);
+    }
+
 </script>
 <template>
     <div class="content-header">
@@ -167,7 +176,7 @@
                                 <td>{{ index+1 }}</td>
                                 <td>{{ user.name }}</td>
                                 <td>{{ user.email }}</td>
-                                <td>--</td>
+                                <td>{{ formatDate(user.created_at) }}</td>
                                 <td>-</td>
                                 <td class="text-center">
                                     <a href="#" @click.prevent="editUser(user)">
